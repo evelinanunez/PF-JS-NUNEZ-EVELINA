0818641962b1267238b8e0379d8a68f79bd1f758
evelinanunez <evenunez@alumno.unlam.edu.ar>
2024-06-19 14:19:55 -0300

    se arregla funcionalidad de agregar al carrito , se mejora el navbar y estilos varios

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //const URL = "https://api-ecommerce-nunez-evelina-default-rtdb.firebaseio.com/";
-
+const carrito = JSON.parse(localStorage.getItem("carrito"))  || []
 
 /* const crearProducto = async (nombre,precio,categoria,stock,descripcion)=>{
     const producto = {
@@ -24,16 +24,17 @@
     const crearCardProducto = (producto) =>{
         const contenedor = document.getElementById("contenedorProductos");;
         const card = document.createElement("div");
-        card.className= "card";
+        //card.id= producto.id;
         card.className= " card estilosCard";
         card.innerHTML = `
-                                    <img src="${producto.imagen}" class="card-img-top" alt="...">
-                                        <div class="card-body">
-                                            <h5 class="card-title">${producto.nombre}</h5>
-                                            <h5>$ ${producto.precio}</h5>
-                                            <p class="card-text">${producto.descripcion}</p>
-                                            <a href="#" class="btn btn-primary">Agregar al Carrito</a>
-                                        </div>
+                        <img src="${producto.imagen}" class="card-img-top" alt="...">
+                        <div class="card-body" id="${producto.id}">
+                            <h5 class="card-title">${producto.titulo}</h5>
+                            <h5>$ ${producto.precio}</h5>
+                            <p class="card-text">${producto.descripcion}</p>
+                            <input class="cantidad" type="number" max="${producto.stock}" min="1" value="1"/>
+                            <button class="btn-agregar-carrito">Agregar al Carrito</button>
+                        </div>
                             `
                             contenedor.append(card);                    
     }
@@ -52,63 +53,76 @@ const obtenerProductos = async() =>{
 }
 
 
-
-
-
-/* const mostrarFormulario = () => {
-    const aplicacion = document.getElementById("app");
-    const element = document.createElement("div");
-    const elementFila = document.createElement("div");
-    element.append(elementFila);
-    elementFila.className= "row justify-content-center";
-    element.className="container";
-    element.id="formulario";
-    elementFila.innerHTML= `
-                            <div class="col-12 col-md-9">
-                            <div class="mb-3">
-                            <label for="nombreProducto" class="form-label">Nombre producto: </label>
-                            <input type="text" class="form-control" id="nombreProducto" placeholder="Indique el nombre del producto">
-                            </div>
-                            <div class="mb-3">
-                            <label for="precioProducto" class="form-label">Precio:</label>
-                            <input type="number" class="form-control" id="precioProducto" placeholder="Coloque un precio para el producto">
-                            </div>
-                            <div class="mb-3">
-                            <label for="categoriaProducto" class="form-label">Categoria de producto:</label>
-                            <input type="text" class="form-control" id="categoriaProducto" placeholder="Seleccione tipo de producto">
-                            </div>
-                            <div class="mb-3">
-                            <label for="formFile" class="form-label">Seleccione una imagen: </label>
-                            <input class="form-control" type="file" id="formFile">
-                            </div>
-                            <div class="mb-3">
-                            <label for="stockProducto" class="form-label">Stock disponible: </label>
-                            <input class="form-control" type="number" id="stockProducto">
-                            </div>
-                            <div class="mb-3">
-                            <label for="descripcionProducto" class="form-label">Descripción del producto:</label>
-                            <textarea class="form-control" id="descripcionProducto" rows="3"></textarea>
-                            </div>
-                            <div class="mb-3 text-center">
-                            <button type="button" class="btn btn-success" id="btnAgregarProducto">Enviar</button>
-                            <button type="button" class="btn btn-dark" id="btnCancelarAgregarProducto">Cancelar</button>
-                            </div>
-  </div>
-                           `
-    aplicacion.append(element);
-} */
-
-/* const verCarrito = () => {
-
-    carrito.forEach(producto => {
-    crearTarjetaProductoCarrito(producto)
+const agregarAlCarrito =(productos)=>{
+    const contenedorProductos = document.getElementById("contenedorProductos");
+    contenedorProductos.addEventListener("click", (ev)=>{
+        if(ev.target && ev.target.classList.contains("btn-agregar-carrito")){
+            const parentElement = ev.target.parentElement;
+            const id = parentElement.id;
+            const cantidad = parentElement.getElementsByClassName("cantidad")[0].value
+            const producto = productos.find(producto => producto.id == id)
+            carrito.push({...producto,cantidad})
+            localStorage.setItem("carrito",JSON.stringify(carrito))
+            Swal.fire("Producto agregado al carrito!"); 
+        }
     })
-}     */
+}
 
+const crearCardProductoCarrito = (producto) =>{
+    const contenedor = document.getElementById("contenedorProductosCarrito");
+    const card = document.createElement("div");
+    card.className= " card estilosCard";
+    card.id= producto.id;
+    card.innerHTML = `
+                                <img src="${producto.imagen}" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${producto.titulo}</h5>
+                                        <h5>$ ${producto.precio}</h5>
+                                        <p class="card-text">${producto.descripcion}</p>
+                                        <div class="d-flex justify-content-center">
+                                        <button class="btn btn-danger btn-eliminar-producto-carrito m-1">Eliminar</button>
+                                        <button class="btn btn-primary btn-comprar m-1">Comprar</button>
+                                        <div>
+                                    </div>
+                        `
+                        contenedor.append(card);                    
+}
+
+const  agregarContenedorDeCarritoVacio= ()=>{
+    const contenedor = document.getElementById("contenedorProductosCarrito");
+    const contenedorMensaje = document.createElement("div");
+    contenedorMensaje.innerHTML =`
+                                    <div class="container mt-5">
+                                        <div class="row justify-content-center">
+                                            <div class="col-md-6 text-center">
+                                                <div class="alert alert-warning" role="alert">
+                                                    <h4 class="alert-heading">Carrito Vacío</h4>
+                                                    <p>Tu carrito está vacío. Añade productos para continuar con tu compra.</p>
+                                                    <hr>
+                                                    <a href="/index.html" class="btn btn-primary">Ir a la tienda</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                 `
+                                 contenedor.append(contenedorMensaje);                             
+}
+
+
+ const verCarrito = () => {
+    console.log(carrito)
+    if(carrito == ''){
+        agregarContenedorDeCarritoVacio();
+    }
+    carrito.forEach(producto => {
+        crearCardProductoCarrito(producto);
+    })
+}     
 
 
 const main = async () => {
     const productos = await obtenerProductos();
+    agregarAlCarrito(productos);
 
 }
 
